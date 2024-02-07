edf83695ccccc02a5afd1c3530ccfcff10b03b9f
ortiz488 <103153174+ortiz488@users.noreply.github.com>
2024-02-06 20:33:00 -0800

    Implemented weather icons

--- app/src/main/java/com/example/myweather/weatherActivity.java
+++ app/src/main/java/com/example/myweather/weatherActivity.java
@@ -7,16 +7,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.squareup.picasso.Picasso;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class weatherActivity extends AppCompatActivity {
 
@@ -52,8 +59,6 @@ public class weatherActivity extends AppCompatActivity {
         //Parse the api response for the forecast weather and display its values
         parseJsonForecast();
 
-        //setGuiForecast();
-
         Button back_btn = findViewById(R.id.back_button);
         Button newSearch_btn = findViewById(R.id.newSearch_button);
 
@@ -88,6 +93,14 @@ public class weatherActivity extends AppCompatActivity {
 
             JsonObject jsonObject2 = (JsonObject)jsonObject1.get("data");
 
+
+            String time = removeQuotes(jsonObject2.get("time").toString());
+            Log.e("Debug", "FROM parseJsonRealTime - Time:  ---> " + time);
+
+            time = toPst(time);
+            Log.e("Debug", "FROM parseJsonRealTime - After toPst - Time:  ---> " + time);
+
+
             JsonObject jsonObject3 = (JsonObject)jsonObject2.get("values");
 
             String temp = toFahrenheit(jsonObject3.get("temperature").toString());
@@ -102,9 +115,10 @@ public class weatherActivity extends AppCompatActivity {
             Log.e("Debug", "FROM parseJsonRealTime - LOCATION:  ---> " + location);
 
 
-            Log.e("Debug", "FROM parseJsonRealTime - BEFORE WeatherDescription() func. call:  ---> ");
-            String weatherDescription = getWeatherCode(weatherCode);
+            String weatherDescription = removeQuotes(getWeatherCode(weatherCode));
             Log.e("Debug", "FROM parseJsonRealTime - Weather Description:  ---> " + weatherDescription);
+
+            setWeatherImage(weatherCode, time);
 
             //Set the textview text for temperature
             TextView temp_textView = findViewById(R.id.temp_textView);
@@ -113,6 +127,10 @@ public class weatherActivity extends AppCompatActivity {
             //Set the textview text for the location
             TextView city_textView = findViewById(R.id.city_textView);
             city_textView.setText(location);
+
+            TextView weatherDescription_textView = findViewById(R.id.weatherDescription_textView);
+            //weatherDescription_textView.setText("Current Weather Description: " + weatherDescription);
+            weatherDescription_textView.setText(weatherDescription);
 
         }catch (Exception e){
             e.printStackTrace();
@@ -277,63 +295,154 @@ public class weatherActivity extends AppCompatActivity {
     }
 
 
+    //Leave the first 10 characters of the string and cut off the remaining characters.
     public String formatDate(String date){
         return date.substring(0, 10);
     }
 
 
+    //This function using the weather code from the realtime api response to read an external file called
+    // "weatherCodes.json". The argument passed is a weather code that is the key. The function returns
+    // the value of the weather code (key).
     public String getWeatherCode(String code){
         String weatherDescription= "ERROR";
         String weatherCodesJson = "";
 
         BufferedReader reader;
 
-        Log.e("Debug", "FROM getWeatherCode - before try block ----->  ");
-
+        //Open the external file found in the assets folder called "weatherCodes.json".
         try {
-            Log.e("Debug", "FROM getWeatherCode - IN try block ----->  ");
             reader = new BufferedReader(new InputStreamReader(getAssets().open("weatherCodes.json"), "UTF-8"));
-            //String newLine = reader.readLine();
             String newLine;
 
+            //Read the file and assign the files contents to the string "newLine"
             while ((newLine = reader.readLine()) != null){
                 weatherCodesJson += newLine;
-                Log.e("Debug", "FROM getWeatherCode - Inside while block - weatherCodes.json: ----->  " + weatherCodesJson);
             }
 
+            //Close the file
             reader.close();
+            //Debug
             Log.e("Debug", "FROM getWeatherCode - weatherCodes.json: ----->  " + weatherCodesJson);
 
         }catch (IOException e){
             e.printStackTrace();
         }
 
-        if(weatherCodesJson != null){
-            Log.e("Debug", "FROM getWeatherCode if block - Before json parse: ----->  ");
-
-            //try
-                JsonParser parser = new JsonParser();
-            Log.e("Debug", "FROM getWeatherCode if block - new json parse: ----->  ");
+        //If the string containing the file contents is not null, then parse the json file
+        // and get the value for the key (weather code).
+        if(weatherCodesJson != null) {
+            JsonParser parser = new JsonParser();
 
             JsonObject jsonObject1 = (JsonObject) parser.parse(weatherCodesJson);
 
-            Log.e("Debug", "FROM getWeatherCode if block - jsonObject1: ----->  ");
-
-
             JsonObject jsonObject2 = (JsonObject) jsonObject1.get("weatherCode");
 
-                weatherDescription = jsonObject2.get(code).toString();
-
-                /*if (weatherDescription == null){
-                    weatherDescription = "ERROR";
-                }*/
-
-            /*}catch (IOException e){
-                e.printStackTrace();
-            }*/
+            weatherDescription = jsonObject2.get(code).toString();
         }
 
         return weatherDescription;
     }
+
+
+    public String toPst(String utc){
+        String pst = "";
+
+        try {
+            SimpleDateFormat utcFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            utcFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Date utcDate = utcFormat.parse(utc);
+
+            SimpleDateFormat pstFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            pstFormat.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+
+            pst = pstFormat.format(utcDate);
+
+            //Debug
+            Log.e("Debug", "FROM toPst - pst time: ----->  " + pst);
+
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        return pst;
+    }
+
+
+    public void setWeatherImage(String code, String time){
+        ImageView weatherImage = findViewById(R.id.weather_imageView);
+
+        String imagePath = "";
+        String weatherCodesPng = "";
+        BufferedReader reader;
+
+        String testURL = "https://raw.githubusercontent.com/Tomorrow-IO-API/tomorrow-weather-codes/master/V2_icons/large/png/10000_clear_large%402x.png";
+
+        //Open the external file found in the assets folder called ""weatherCodePng".json" and read the file contents into the string "weatherCodesPng"
+        try {
+            reader = new BufferedReader(new InputStreamReader(getAssets().open("weatherCodePng.json"), "UTF-8"));
+            String newLine;
+
+            //Read the file and assign the files contents to the string "newLine"
+            while ((newLine = reader.readLine()) != null){
+                weatherCodesPng += newLine;
+            }
+            //Close the file
+            reader.close();
+
+            //Debug
+            Log.e("Debug", "FROM setWeatherImage - weatherCodePng.json: ----->  " + weatherCodesPng);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        //If the string containing the file contents is not null, then parse the json file
+        // and get the value for the key (weather code).
+        if(weatherCodesPng != null) {
+
+            //int hoursInt;
+            String hoursStr = time.substring(11,13);
+            int hoursInt = Integer.parseInt(hoursStr);
+            Log.e("Debug", "FROM setWeatherImage - get hours: ----->  " + hoursStr);
+
+
+            if(hoursInt >= 17) {
+                switch (code)
+                {
+                    case "1000":
+                        code = "10001";
+                        break;
+                    case "1102":
+                        code = "11021";
+                        break;
+                    case "1101":
+                        code = "11011";
+                        break;
+                    case "1100":
+                        code = "11001";
+                        break;
+                }
+                //Debug
+                Log.e("Debug", "FROM setWeatherImage - if block - New Code: ----->  " + code);
+            }
+
+            JsonParser parser = new JsonParser();
+
+            JsonObject jsonObject1 = (JsonObject) parser.parse(weatherCodesPng);
+
+            JsonObject jsonObject2 = (JsonObject) jsonObject1.get("weatherCodePng");
+
+            imagePath = removeQuotes(jsonObject2.get(code).toString());
+
+            //Debug
+            Log.e("Debug", "FROM setWeatherImage - image path: ----->  " + imagePath);
+        }
+
+        Picasso.get().load(imagePath).into(weatherImage);
+
+        //Picasso.get().load(testURL).into(weatherImage);
+    }
+
+    //android:background="@drawable/weather5"
 
 }
